c5a933bcfb129620e4469e7ed4a41554d3b6b549
Rudransh Shrivastava <rudransh.shr@gmail.com>
2023-08-16 12:19:12 +0530

    added a player and buttons

--- script.js
+++ script.js
@@ -1,33 +1,72 @@
 document.addEventListener("DOMContentLoaded", function() {
     const youtubeForm = document.getElementById("youtubeForm");
     const youtubeUrlInput = document.getElementById("youtubeUrl");
+    const playerContainer = document.getElementById("playerContainer");
+    let player;
 
     youtubeForm.addEventListener("submit", function(event) {
         event.preventDefault();
         const youtubeUrl = youtubeUrlInput.value;
         if (youtubeUrl) {
-            // Call a function to handle the YouTube URL
-            handleYouTubeUrl(youtubeUrl);
+            const videoId = extractVideoId(youtubeUrl);
+            if (videoId) {
+                // Remove the form
+                youtubeForm.style.display = "none";
+
+                // Load the YouTube video
+                loadYouTubeVideo(videoId);
+            }
         }
     });
+    // Play button
+    const playButton = document.getElementById('playButton');
+    playButton.addEventListener('click', function(event) {
+        console.log("play");
+        event.preventDefault();
+        player.playVideo();   
+    });
+    // Pause button
+    const pauseButton = document.getElementById('pauseButton');
+    pauseButton.addEventListener('click', function(event) {
+        console.log("pause");
+        event.preventDefault();
+        player.pauseVideo();
+    });
+    // Progress bar and seeking
+    const progressBar = document.getElementById('progressBar');
 
-    function handleYouTubeUrl(url) {
-        // Here you can implement the logic to process the YouTube URL
-        // For now, let's just log the URL to the console
-        const videoID = extractVideoId(url)
-        if(videoID == null){
-            // send a message that error
-            console.log("error");
-        }else {
-            // handle 
-            console.log(videoID);
-        }
+    // Update progress bar as the video plays
+    player.addEventListener('onStateChange', function(event) {
+    if (event.data === YT.PlayerState.PLAYING) {
+        setInterval(function() {
+            console.log("playing");
+            const currentTime = player.getCurrentTime();
+            const duration = player.getDuration();
+            const progress = (currentTime / duration) * 100;
+            progressBar.style.width = `${progress}%`;
+        }, 1000); // Update every second
     }
+    });
+
     function extractVideoId(url) {
     // Extract the video ID from the YouTube URL
     // You can use regular expressions or other methods to extract the ID
     // URL format: https://www.youtube.com/watch?v=VIDEO_ID
         const match = url.match(/(?:\?v=|\/embed\/|\/\d\/|\/vi\/|\/e\/|\/v\/|https:\/\/youtu.be\/)([a-zA-Z0-9_-]{11})/);
         return match ? match[1] : null;
+    }
+    function loadYouTubeVideo(videoId) {
+        playerContainer.innerHTML = ""; // Clear any previous content
+        player = new YT.Player("playerContainer", {
+            height: "315",
+            width: "560",
+            videoId: videoId,
+            // events: {
+            //     onReady: onPlayerReady,
+            // },
+        });
+    }
+    function onPlayerReady(event) {
+    event.target.playVideo();
     }
 });
