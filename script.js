a0593a9fc1dbd0a083ee731595205d1b1a84e910
Rudransh Shrivastava <rudransh.shr@gmail.com>
2023-08-16 15:06:54 +0530

    added progress bar

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const youtubeForm = document.getElementById("youtubeForm");
     const youtubeUrlInput = document.getElementById("youtubeUrl");
     const playerContainer = document.getElementById("playerContainer");
     let player;
+    let playerInterval;
 
-    youtubeForm.addEventListener("submit", function(event) {
+    youtubeForm.addEventListener("submit", function (event) {
         event.preventDefault();
         const youtubeUrl = youtubeUrlInput.value;
         if (youtubeUrl) {
@@ -20,53 +21,71 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     // Play button
     const playButton = document.getElementById('playButton');
-    playButton.addEventListener('click', function(event) {
+    playButton.addEventListener('click', function (event) {
         console.log("play");
         event.preventDefault();
-        player.playVideo();   
+        player.playVideo();
     });
     // Pause button
     const pauseButton = document.getElementById('pauseButton');
-    pauseButton.addEventListener('click', function(event) {
+    pauseButton.addEventListener('click', function (event) {
         console.log("pause");
         event.preventDefault();
         player.pauseVideo();
     });
-    // Progress bar and seeking
-    const progressBar = document.getElementById('progressBar');
 
-    // Update progress bar as the video plays
-    player.addEventListener('onStateChange', function(event) {
-    if (event.data === YT.PlayerState.PLAYING) {
-        setInterval(function() {
-            console.log("playing");
-            const currentTime = player.getCurrentTime();
-            const duration = player.getDuration();
-            const progress = (currentTime / duration) * 100;
-            progressBar.style.width = `${progress}%`;
-        }, 1000); // Update every second
-    }
-    });
 
     function extractVideoId(url) {
-    // Extract the video ID from the YouTube URL
-    // You can use regular expressions or other methods to extract the ID
-    // URL format: https://www.youtube.com/watch?v=VIDEO_ID
+        // Extract the video ID from the YouTube URL
+        // You can use regular expressions or other methods to extract the ID
+        // URL format: https://www.youtube.com/watch?v=VIDEO_ID
         const match = url.match(/(?:\?v=|\/embed\/|\/\d\/|\/vi\/|\/e\/|\/v\/|https:\/\/youtu.be\/)([a-zA-Z0-9_-]{11})/);
         return match ? match[1] : null;
     }
+    // Progress bar and seeking
+    const progressBar = document.getElementById('progressBar');
+
     function loadYouTubeVideo(videoId) {
         playerContainer.innerHTML = ""; // Clear any previous content
         player = new YT.Player("playerContainer", {
             height: "315",
             width: "560",
             videoId: videoId,
-            // events: {
-            //     onReady: onPlayerReady,
-            // },
+            events: {
+                onStateChange: onStateChange,
+            },
         });
     }
-    function onPlayerReady(event) {
-    event.target.playVideo();
+
+    function onStateChange(event) {
+        if (event.data === YT.PlayerState.PLAYING) {
+            const interval = setInterval(function () {
+                const currentTime = player.getCurrentTime();
+                const duration = player.getDuration();
+                const progress = (currentTime / duration) * 100;
+                progressBar.style.width = `${progress}%`;
+            }, 250);
+
+            // Store the interval ID so you can clear it later
+            playerInterval = interval;
+        } else {
+            // Clear the interval when the video is paused or ended
+            clearInterval(playerInterval);
+        }
     }
+    const progressContainer = document.getElementById('progressContainer');
+
+    progressContainer.addEventListener('click', function (event) {
+        const progressBar = document.getElementById('progressBar');
+        const progressContainer = document.getElementById('progressContainer');
+        const progressBarWidth = progressBar.offsetWidth; // current progress
+        const offsetX = event.offsetX; // x off set from the left
+        const totalBarWidth = progressContainer.offsetWidth; // total width of the bar
+        const duration = player.getDuration(); // total time of the video in sec
+        const progressPercent = offsetX / totalBarWidth * 100; // percent progress
+        const seekTime = duration * progressPercent / 100;
+
+        player.seekTo(seekTime, true);
+    });
+
 });
